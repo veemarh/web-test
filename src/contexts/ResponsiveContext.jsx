3f85c3b1cbbc33f1151cf1d0366e2e986273466b
veta <vee.marh.my@gmail.com>
2025-01-28 14:18:20 +0300

    react responsive added

---
+++ src/contexts/ResponsiveContext.jsx
@@ -0,0 +1,18 @@
+import {createContext, useContext} from 'react';
+import {useMediaQuery} from 'react-responsive';
+
+export const ResponsiveContext = createContext(undefined);
+
+export const ResponsiveContextProvider = ({children}) => {
+    const isDesktop = useMediaQuery({minWidth: 992});
+    const isMobile = useMediaQuery({maxWidth: 767});
+    const isTablet = !isDesktop && !isMobile;
+
+    return (
+        <ResponsiveContext.Provider value={{isDesktop, isMobile, isTablet}}>
+            {children}
+        </ResponsiveContext.Provider>
+    );
+};
+
+export const useResponsiveContext = () => useContext(ResponsiveContext);
