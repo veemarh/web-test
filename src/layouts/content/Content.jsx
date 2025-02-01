a7c819a46f98a2896cdc81c3d601a196a7bb90ec
veta <vee.marh.my@gmail.com>
2025-02-01 14:40:37 +0300

    shadow when scroll added

--- src/layouts/content/Content.jsx
+++ src/layouts/content/Content.jsx
@@ -1,11 +1,23 @@
 import styles from "./content.module.css";
 import DefaultButton from "../../components/default-button/DefaultButton.jsx";
 import Form from "./Form.jsx";
+import {useRef, useState} from "react";
 
 export default function Content() {
+    const [shadow, setShadow] = useState(false);
+    const containerRef = useRef(null);
+
+    const handleScroll = () => {
+        if (containerRef.current.scrollTop > 0) {
+            setShadow(true);
+        } else {
+            setShadow(false);
+        }
+    };
+
     return (
         <section className={styles.content}>
-            <header className={styles.subheadingAndPadding}>
+            <header className={`${styles.subheadingAndPadding} ${shadow ? styles.shadow : ""}`}>
                 <div className={styles.leftSide}>
                     <h2 className={styles.subheadingTitle}>Подзадача</h2>
                     <DefaultButton text="Создать"/>
@@ -15,8 +27,9 @@ export default function Content() {
                     <DefaultButton text="Сохранить и выйти"/>
                 </div>
             </header>
-            <article className={styles.paddingForm}>
-                <h1 className={`${styles.mainTitle} ${styles.overflowText}`}>STSK0004783 На инциденте, запросе, проблеме,
+            <article ref={containerRef} onScroll={handleScroll} className={styles.paddingForm}>
+                <h1 className={`${styles.mainTitle} ${styles.overflowText}`}>STSK0004783 На инциденте, запросе,
+                    проблеме,
                     в статусе закрыто некоторые поля остаются редактируемыми для агента если он Caller</h1>
                 <Form/>
             </article>
