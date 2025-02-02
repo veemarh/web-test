1c1764489672dcf138e18c2bd55a1a4eed15b8af
veta <vee.marh.my@gmail.com>
2025-02-02 21:35:09 +0300

    form for mobile

--- src/layouts/content/Content.jsx
+++ src/layouts/content/Content.jsx
@@ -1,14 +1,15 @@
 import styles from "./content.module.css";
 import DefaultButton from "../../components/default-button/DefaultButton.jsx";
 import Form from "./Form.jsx";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useResponsiveContext} from "../../contexts/ResponsiveContext.jsx";
 import {useModal} from "../../hooks/useModal.jsx";
 import Modal from "../../components/modal/Modal.jsx";
 
-export default function Content({navigatorVisible, isForm = false}) {
+export default function Content({navigatorVisible, isForm = false, onClose}) {
     const [shadow, setShadow] = useState(false);
     const containerRef = useRef(null);
+    const contentRef = useRef(null);
     const {isMobile} = useResponsiveContext();
     const {isOpen, openModal, closeModal} = useModal();
     const extraContentProps = isMobile || isForm ? {"data-mobile": true} : {};
@@ -22,28 +23,45 @@ export default function Content({navigatorVisible, isForm = false}) {
         }
     };
 
+    useEffect(() => {
+        if (isOpen && contentRef.current && !isForm) {
+            contentRef.current.setAttribute("data-hidden", true);
+        } else if (contentRef.current) {
+            contentRef.current.removeAttribute("data-hidden");
+        }
+    }, [isOpen, isForm]);
+
     return (
-        <section {...extraContentProps}
-                 className={`${styles.content} ${navigatorVisible ? styles.withNavigatorVisible : styles.withNavigatorInvisible}`}>
+        <section ref={contentRef} {...extraContentProps}
+                 className={`${styles.content} ${navigatorVisible ? styles.withNavigatorVisible : styles.withNavigatorInvisible} ${isOpen && !isForm && styles.hidden}`}>
             <header {...extraHeaderProps}
                     className={`${styles.subheadingAndPadding} ${shadow ? styles.shadow : ""} ${navigatorVisible ? styles.withNavigatorVisible : styles.withNavigatorInvisible}`}>
                 <div className={`${styles.leftSide} ${isMobile && styles.mobileSide}`}>
-                    <h2 className={styles.subheadingTitle}>Подзадачи</h2>
-                    <DefaultButton text="Создать" onClick={openModal}/>
+                    <h2 className={styles.subheadingTitle}>Подзадача</h2>
+                    {!isForm && <DefaultButton text="Создать" onClick={openModal}/>}
                 </div>
-                {!isMobile && <div className={styles.rightSide}>
-                    <DefaultButton text="Сохранить" option="primary"/>
-                    <DefaultButton text="Сохранить и выйти"/>
-                </div>}
+                <div className={styles.rightSide}>
+                    {(!isMobile || isForm) &&
+                        <>
+                            <DefaultButton text="Сохранить" option="primary"/>
+                            {isForm
+                                ? <DefaultButton text="Отменить" onClick={onClose}/>
+                                : <DefaultButton text="Сохранить и выйти"/>
+                            }
+                        </>
+                    }
+                </div>
             </header>
             <article ref={containerRef} onScroll={handleScroll} className={styles.paddingForm}>
-                <h1 className={`${styles.mainTitle} ${styles.overflowText}`}>STSK0004783 На инциденте, запросе,
-                    проблеме,
-                    в статусе закрыто некоторые поля остаются редактируемыми для агента если он Caller</h1>
+                <h1 className={`${styles.mainTitle} ${styles.overflowText}`}>
+                    {isForm
+                        ? "Новая запись"
+                        : "STSK0004783 На инциденте, запросе, проблеме, в статусе закрыто некоторые поля остаются редактируемыми для агента если он Caller"
+                    }</h1>
                 <Form/>
             </article>
             <Modal isOpen={isOpen} onClose={closeModal}>
-                <Content isForm={true}/>
+                <Content isForm={true} onClose={closeModal}/>
             </Modal>
         </section>
     )
